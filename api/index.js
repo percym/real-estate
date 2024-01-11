d20aa3f7c1a7915e1af734bd8c795b16eef7efa7
Percy Mugadza <percymdev@gmail.com>
2024-01-11 18:54:40 +0000

    init commit

---
+++ api/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.routes.js';
+import authRouter from './controller/auth.controller.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json())
+const PORT =process.env.PORT || 5001;
+
+mongoose.connect(process.env.MONGO)
+.then(()=>app.listen(PORT,()=>console.log(`server running on port ${PORT}`)))
+.catch((error)=> console.log('erar => ', error.message));
+
+app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
+
+//more proffessional middleware

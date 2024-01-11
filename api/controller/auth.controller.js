9867d3b76724fad0dcc00826f9c5e3a134ce1be3
Percy Mugadza <percymdev@gmail.com>
2024-01-11 23:07:12 +0000

    created sign in page ui

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -11,7 +11,8 @@ const signup = async (req, res,next) => {
         await newUser.save();
         res.status(201).json('User created successfully');
     } catch (error) {
-        next(error);
+        // next(errorHandler(550,'error from the function'))
+       next(error);
     }
 }
 

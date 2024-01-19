4fa837cab1a76fbe0a337cb43c01d17a49d91ab4
Percy Mugadza <percymdev@gmail.com>
2024-01-19 14:13:31 +0000

    made profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,18 +6,21 @@ import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import About from './pages/About';
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
-  return ( 
-      <BrowserRouter>
-        <Header/>
-        <Routes>
-          <Route path="/"  element={<Home />}/>
-          <Route path="/sign-up"  element={<SignUp />}/>
-          <Route path="/sign-in"  element={<SignIn />}/>
-          <Route path="/profile"  element={<Profile />}/>
-          <Route path="/about"  element={<About />}/>
-        </Routes>
-      </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route element={<PrivateRoute />} >
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }

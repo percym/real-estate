c4e9e5daee3013ec9f97eb521ea97a98298f3b1b
Percy Mugadza <percymdev@gmail.com>
2024-02-03 20:15:18 +0000

    refimages uploading of properties

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,6 +1,52 @@
+import { getDownloadURL, ref, getStorage, uploadBytesResumable } from 'firebase/storage';
 import React from 'react';
+import { useState } from 'react';
+import app from '../firebase';
 
 export default function CreateListing() {
+    const [files,setFiles] = useState([]);
+    const [formData, setFormData] = useState({
+        imageUrls:[],
+
+    });
+    console.log(formData);
+    const handleImageSubmit=()=>{
+        if(files.length > 0 && files.length < 7){
+            const promises = [];
+
+            for(let i=0 ; i < files.length ; i++){
+                promises.push(storeImage(files[i]))
+            }
+            Promise.all(promises).then((urls)=>{
+               setFormData({...formData, imageUrls:formData.imageUrls.concat(urls)});     
+            });
+        }
+    }
+
+    const storeImage = async (file)=>{
+        return new Promise((resolve,reject)=>{
+            const storage = getStorage(app);
+            const fileName = new Date().time + file.name;
+            const storageRef = ref(storage,fileName);
+            const uploadTask = uploadBytesResumable(storageRef,file);
+            uploadTask.on(
+                "stateChanged",
+                (snapshot)=>{
+                    const progress = (snapshot.bytesTransferred/ snapshot.totalBytes) * 100;
+                    console.log(`upload progress  ${progress}`);
+                },
+                (error)=>{
+                    reject(error);
+                },
+                ()=>{
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl)=>{
+                        resolve(downloadUrl);
+                    });
+                }
+            );
+
+        });
+    }
     return (
         <main className='p-3 max-w-4xl mx-auto'>
             <h1 className='text-3xl font-semibold text-center my-7'>Create a listing </h1>
@@ -80,9 +126,9 @@ export default function CreateListing() {
                     <p className='font-semibold'>Images:</p>
                     <span className='font-normal text-gray-600 ml-2'>The first image will be the cover</span>
                     <div className='flex gap-4'>
-                        <input className='p-3 border border-gray-300 rounded w-full'
-                             type='file' id='images' accept='images/*' multiple/>
-                        <button className='p-3 text-green-700  rounded uppercase border border-green-700 hover:shadow-lg disabled:opacity-80'>Upload</button>
+                        <input  onChange={(e)=>{setFiles(e.target.files)}} className='p-3 border border-gray-300 rounded w-full'
+                             type='file' id='images' accept='images/*' multiple />
+                        <button type='button' onClick={handleImageSubmit} className='p-3 text-green-700  rounded uppercase border border-green-700 hover:shadow-lg disabled:opacity-80'>Upload</button>
                     </div>
                     <button className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:placeholder-opacity-95 disabled:placeholder-opacity-80'>Create Listing</button>
                 </div>

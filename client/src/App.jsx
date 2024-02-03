ea3acdb8bf3a67552fc65d46fb34608c5bbe675f
Percy Mugadza <percymdev@gmail.com>
2024-02-03 18:38:30 +0000

    complete frontend create-listing- ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Profile from './pages/Profile';
 import About from './pages/About';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
+import CreateListing from './pages/CreateListing';
 
 export default function App() {
   return (
@@ -16,10 +17,11 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/about" element={<About />} />
         <Route element={<PrivateRoute />} >
+          <Route path="/create-listing" element={<CreateListing />} />
           <Route path="/profile" element={<Profile />} />
         </Route>
-        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   )
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,6 +14,7 @@ import {
     signOutUserSuccess,
     signOutUserFailure
    } from '../redux/user/userSlice';
+import { Link } from 'react-router-dom';
 
 export default function Profile() {
 
@@ -163,6 +164,9 @@ export default function Profile() {
 
         <button  disabled={loading} className='bg-slate-700 text-white p-3 
         rounded-lg uppercase hover:opacity-90 disabled:opacity-80'> {loading ? 'Loading...' : 'Update'}</button>
+        <Link   className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:placeholder-opacity-95' to={'/create-listing'}>
+            Create Listing
+        </Link>
       </form>
       <div className='flex justify-between mt-5'>
         <span className='text-red-700 cursor-pointer' onClick={handleUserDelete} >Delete account </span>

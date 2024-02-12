9ba7022dc9a6cea8c4a23a276c29baaeaae0b60c
Percy Mugadza <percymdev@gmail.com>
2024-02-12 09:16:39 +0000

    update user listing api working

--- api/controller/listing.controler.js
+++ api/controller/listing.controler.js
@@ -31,3 +31,26 @@ export const deleteListing =async (req,res,next )=>{
         next(error);
     }
 }
+
+export const updateListing = async (req,res,next)=>{
+    const listing = await Listing.findById(req.params.id);
+    if(!listing){
+        return next(Error(404,'Listing not found'));
+    }
+
+    if(req.user.id !== listing.userRef){
+        return next(Error(404,'You can only update your listing'));
+    }
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new:true}
+        )
+        res.status(200).json(updatedListing);
+        
+    } catch (error) {
+        next(error);
+        
+    }
+}

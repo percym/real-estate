6f6479fadbe90a35bc38a22e47a082adb06f7c80
Percy Mugadza <percymdev@gmail.com>
2024-02-12 23:11:51 +0000

    ready for render

--- api/index.js
+++ api/index.js
@@ -5,8 +5,11 @@ import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.routes.js';
 import cookieParser from 'cookie-parser';
+import path from path;
 
 dotenv.config();
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -20,11 +23,18 @@ mongoose.connect(process.env.MONGO)
 .then(()=>app.listen(PORT,()=>console.log(`server running on port ${PORT}`)))
 .catch((error)=> console.log('erar => ', error.message));
 
+
+
 app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
 app.use('/api/listing',listingRouter);
 
 //to do more professional middleware
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname,'/client/dist/index.html'));
+});
 
 app.use((err,req,res,next)=>{
     const statusCode= err.statusCode ||500;
